6cc05f95f01d77900d789af7b8540d7923019e72
JANU-JANHAVI <84670122+JANU-JANHAVI@users.noreply.github.com>
2022-08-10 04:24:27 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
   declarations: [AppComponent, ProductsComponent],
   imports: [
     BrowserModule,
+        HttpClientModule,
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
